aa482e5e0e9ac802d4064ebfe12c15a06d3c93d7
daniel <daniel@familie-gross.at>
2020-01-10 07:36:37 +0100

    stop motors on shutdown;  Cancle approach if wall in front

--- src/moveToGoal.py
+++ src/moveToGoal.py
@@ -1,18 +1,22 @@
 #! /usr/bin/env python
 
 import rospy
+import numpy as np
+import math
 from std_msgs.msg import String
 from math import pow, atan2, sqrt
 from geometry_msgs.msg._Twist import Twist
 from geometry_msgs.msg._Pose import Pose
 from nav_msgs.msg._Odometry import Odometry
 from std_msgs.msg._Bool import Bool
+from sensor_msgs.msg._LaserScan import LaserScan
 import tf
 
 
 class MoveToGoal:
 
 	def __init__(self):
+		rospy.on_shutdown(self._shutdown)
 		rospy.init_node('move_to_goal')
 		self.pub = rospy.Publisher('phrases', String, queue_size=10)
 		
@@ -27,12 +31,36 @@ class MoveToGoal:
 		
 		self.goal_reached_publisher = rospy.Publisher('move_to_goal/reached', 
 													Bool, queue_size=1)
+		self.scanSub = rospy.Subscriber('/scan', LaserScan, self._scan_callback)
 
+		self.stop = False
 		self.distance_tolerance = 0.03
 		self.pose = Pose()
 		self.rate = rospy.Rate(20)
+		self.obstacle = False
+		self.oldtheta = 0
+
 		rospy.spin()
 
+	def _shutdown(self):
+		self.stop = True
+		vel_msg = Twist()
+		# Linear velocity in the x-axis.
+		vel_msg.linear.x = 0
+		vel_msg.linear.y = 0
+		vel_msg.linear.z = 0
+
+		# Angular velocity in the z-axis.
+		vel_msg.angular.x = 0
+		vel_msg.angular.y = 0
+		self.velocity_publisher.publish(vel_msg)
+		self.rate.sleep()
+
+	def _scan_callback(self, scan):
+		if scan.ranges[0] < 0.2:
+			self.obstacle = True
+		else:
+			self.obstacle = False
 
 	def _update_pose(self, data):
 		"""
@@ -43,6 +71,12 @@ class MoveToGoal:
 		self.pose.position.y = round(data.pose.pose.position.y, 4)
 		self.robot_yaw = self._robot_angle()
 
+	def _robot_angle(self):
+		orientation_q = self.pose.orientation
+		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+		(roll, pitch, yaw) = tf.transformations.euler_from_quaternion(orientation_list)
+		return yaw
+
 	def _update_goal(self, goal_pose):
 		"""
 		New goal to approach
@@ -52,36 +86,30 @@ class MoveToGoal:
 
 		vel_msg = Twist()
 
-		while self._euclidean_distance(goal_pose) >= self.distance_tolerance:
-			if abs(self._steering_angle(goal_pose) - self.robot_yaw) > 0.1:
+		cancle = False
+
+		while self._euclidean_distance(goal_pose) >= self.distance_tolerance and not self.stop and not cancle:
+			# Linear velocity in the x-axis.
+			vel_msg.linear.x = 0
+			vel_msg.linear.y = 0
+			vel_msg.linear.z = 0
+
+			# Angular velocity in the z-axis.
+			vel_msg.angular.x = 0
+			vel_msg.angular.y = 0
+
+			if abs(self._steering_angle(goal_pose) - self.robot_yaw) > 0.05:
 				print('rotate')
-				# Linear velocity in the x-axis.
-				vel_msg.linear.x = 0
-				vel_msg.linear.y = 0
-				vel_msg.linear.z = 0
-
-				# Angular velocity in the z-axis.
-				vel_msg.angular.x = 0
-				vel_msg.angular.y = 0
-				if self._angular_vel(goal_pose) > 0:
-					vel_msg.angular.z = 0.2
-				else:
-					vel_msg.angular.z = -0.2
-				
-				# vel_msg.angular.z = self._angular_vel(goal_pose)
-
+				vel_msg.angular.z = self._angular_vel(goal_pose)
 			else:
 				print('Forward')
-				# Linear velocity in the x-axis.
-				vel_msg.linear.x = self._linear_vel(goal_pose)
-				#vel_msg.linear.x = 0.2
-				vel_msg.linear.y = 0
-				vel_msg.linear.z = 0
-
-				# Angular velocity in the z-axis.
-				vel_msg.angular.x = 0
-				vel_msg.angular.y = 0
-				vel_msg.angular.z = 0
+				if not self.obstacle:
+					vel_msg.linear.x = self._linear_vel(goal_pose)
+				else:
+					cancle = True
+					vel_msg.linear.x = 0
+					vel_msg.angular.z = 0
+					self.velocity_publisher.publish(vel_msg)
 
 			# Publishing our vel_msg
 			self.velocity_publisher.publish(vel_msg)
@@ -97,30 +125,41 @@ class MoveToGoal:
 		vel_msg.angular.z = 0
 		self.velocity_publisher.publish(vel_msg)
 		self.rate.sleep()
-		self.rate.sleep()
-		self.rate.sleep()
-		self.goal_reached_publisher.publish(True)
+		if not cancle:
+			self.goal_reached_publisher.publish(True)
+		else:
+			self.goal_reached_publisher.publish(False)
 
 	def _euclidean_distance(self, goal_pose):
 		return sqrt(pow((goal_pose.position.x - self.pose.position.x), 2) +
 					pow((goal_pose.position.y - self.pose.position.y), 2))
 
-	def _linear_vel(self, goal_pose, constant=0.4):
+	def _linear_vel(self, goal_pose, constant=0.3):
 		return constant * self._euclidean_distance(goal_pose)
 
 	def _steering_angle(self, goal_pose):
-		return atan2(goal_pose.position.y - self.pose.position.y, goal_pose.position.x - self.pose.position.x)
-
-	def _robot_angle(self):
-		orientation_q = self.pose.orientation
-		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-		(roll, pitch, yaw) = tf.transformations.euler_from_quaternion(orientation_list)
-		return yaw
+		y = goal_pose.position.y - self.pose.position.y
+		x = goal_pose.position.x - self.pose.position.x
+		
+		theta = atan2(y, x)
+		
+		return theta
 
 	def _angular_vel(self, goal_pose, constant=0.3):
 		yaw = self._robot_angle()
 		steer_angle = self._steering_angle(goal_pose)
 		return constant * (steer_angle - yaw)
+		#steer_angle = (steer_angle + 2*math.pi) % 2*math.pi
+		#yaw = (yaw + 2*math.pi) % 2*math.pi
+
+		#angle = 0
+
+		#if steer_angle > yaw:
+		#	angle = steer_angle - yaw
+		#else:
+		#	angle = yaw - steer_angle
+
+		#return constant * (angle - math.pi)
 
 if __name__ == '__main__':
 	try:
